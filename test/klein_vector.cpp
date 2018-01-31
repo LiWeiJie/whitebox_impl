65d2c4ad8d6eee8555d5904fbb72cadaa6d21fa6
liweijie <licatweijie@gmail.com>
2018-01-31 15:40:35 +0100

    add cycles count in klein test

--- test/klein_vector.cpp
+++ test/klein_vector.cpp
@@ -6,139 +6,12 @@
 
 #include <klein/klein80wb.h>
 
+#include "count_cycles.h"
+
 #define EXPERIMENT_TIMES 1
-
-void klein_testvectors()
-{
-
-	uint8_t i;
-
-	//test values
-	const uint8_t key8_1[8] = {0};
-
-	const uint8_t key8_2[8] = {
-	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-	};
-
-	const uint8_t key8_3[8] = {
-	0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF,
-	};
-
-
-	const uint8_t key10_1[10] = {0};
-
-	const uint8_t key10_2[10] = {
-	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-	};
-
-	const uint8_t key10_3[10] = {
-	0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0x12, 0x34,
-	};
-
-
-	const uint8_t key12_1[12] = {0};
-
-	const uint8_t key12_2[12] = {
-	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-	};
-
-	const uint8_t key12_3[12] = {
-	0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56, 0x78,
-	};
-
-	uint8_t message1[8] = {0};
-
-	uint8_t message2[8] = {
-	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-	};
-
-	uint8_t message3[8] = {
-	0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF,
-	};
-
-	uint8_t cipher[8];
-
-	printf("============klein-64 test vector===========\n");
-	klein64_encrypt(message2, key8_1, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-	klein64_encrypt(message1, key8_2, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-	klein64_encrypt(message2, key8_3, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-	klein64_encrypt(message3, key8_1, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-	printf("============klein-80 test vector===========\n");
-	klein80_encrypt(message2, key10_1, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-	klein80_encrypt(message1, key10_2, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-	klein80_encrypt(message2, key10_3, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-	klein80_encrypt(message3, key10_1, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-	printf("============klein-96 test vector===========\n");
-	klein96_encrypt(message2, key12_1, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-	klein96_encrypt(message1, key12_2, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-	klein96_encrypt(message2, key12_3, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-	klein96_encrypt(message3, key12_1, cipher);
-	for (i = 0; i < 8; i++) {
-	    printf("%02X ", cipher[i]);
-	}
-	printf("\n");
-
-}
 
 int klein_stand()
 {
-
-	//klein_testvectors();
 
     //test values
 	const uint8_t key8[8] = {
@@ -173,20 +46,21 @@ int klein_stand()
 	
 	clock_t program_start, program_end = 0;
 	program_start = clock();
+	set_cycles_start();
 
 	for(i = 0; i < EXPERIMENT_TIMES; i++)
 	{
-        //klein64_encrypt(message, key8, cipher);
 	    klein80_encrypt(message, key10, cipher);
-	    //klein96_encrypt(message, key12, cipher);
     }
 
+	set_cycles_ends();
 	program_end = clock();
 
 	double ts = program_end-program_start;
 	ts = ts*1000/CLOCKS_PER_SEC;
 
 	printf("The %d times encryptions spend %lf ms\n", i, ts);
+	printf("The %d times encryptions spend %llu cycles\n", i, get_cycles_elapsed());
 
 	for (i = 0; i < 8; i++) {
 	    printf("%02X ", cipher[i]);
@@ -195,19 +69,21 @@ int klein_stand()
 	printf("\n");
 
 	program_start = clock();
+	set_cycles_start();
+	
 
 	for(i = 0; i < EXPERIMENT_TIMES; i++)
 	{
-		//klein64_decrypt(cipher, key8, message);
 	    klein80_decrypt(cipher, key10, message);
-		//klein96_decrypt(cipher, key12, message);
 	}
 
+	set_cycles_ends();
 	program_end = clock();
 	ts = program_end-program_start;
 	ts = ts*1000/CLOCKS_PER_SEC;
 
 	printf("The %d times decryptions spend %lf ms\n", i, ts);
+	printf("The %d times decryptions spend %llu cycles\n", i, get_cycles_elapsed());
 
 	for (i = 0; i < 8; i++) {
 		printf("%02X ", message[i]);
@@ -259,16 +135,20 @@ int klein_wb()
 	int i;
 	clock_t program_start, program_end = 0;
 	program_start = clock();
+	set_cycles_start();
 
 	gen_klein80wb_table(key10, &wb_ctx);
-
+	
+	set_cycles_ends();
 	program_end = clock();
 	double ts = program_end - program_start;
 	ts = ts*1000/CLOCKS_PER_SEC;
 
 	printf("The generating of encryptions table spend %lf ms\n", ts);
+	printf("The generating of encryptions table spend %llu cycles\n", get_cycles_elapsed());
 	
 	program_start = clock();
+	set_cycles_start();
 
 	for(i = 0; i < EXPERIMENT_TIMES; i++)
 	{
@@ -277,12 +157,14 @@ int klein_wb()
 	    //klein96_encrypt(message, key12, cipher);
     }
 
+	set_cycles_ends();
 	program_end = clock();
 
 	ts = program_end - program_start;
 	ts = ts*1000/CLOCKS_PER_SEC;
 
 	printf("The %d times encryptions spend %lf ms\n", i, ts);
+	printf("The %d times encryptions spend %llu cycles\n", i, get_cycles_elapsed());
 
 	for (i = 0; i < 8; i++) {
 	    printf("%02X ", cipher[i]);
