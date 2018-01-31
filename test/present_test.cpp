a09dbc403fe04bd8e6c7a1e9c1a396da364abba5
liweijie <licatweijie@gmail.com>
2018-01-31 23:08:10 +0100

    add cycle count in present

--- test/present_test.cpp
+++ test/present_test.cpp
@@ -2,13 +2,15 @@
  * @Author: Weijie Li 
  * @Date: 2018-01-29 17:29:57 
  * @Last Modified by: Weijie Li
- * @Last Modified time: 2018-01-31 23:03:56
+ * @Last Modified time: 2018-01-31 23:07:48
  */
 
 #include <stdio.h>
 #include <time.h>
 #include <present/present80.h>
 #include <present/present80wb.h>
+
+#include "count_cycles.h"
 
 #define EXPERIMENT_TIMES 1
 
@@ -34,18 +36,21 @@ int present80_stand()
 	
 	clock_t program_start, program_end = 0;
 	program_start = clock();
+	set_cycles_start();
 
 	for(i = 0; i < EXPERIMENT_TIMES; i++)
 	{
 	    present(message, key10, cipher);
     }
 
+	set_cycles_ends();
 	program_end = clock();
 
 	double ts = program_end-program_start;
 	ts = ts*1000/CLOCKS_PER_SEC;
 
 	printf("The %d times encryptions spend %lf ms\n", i, ts);
+	printf("The %d times encryptions spend %llu cycles\n", i, get_cycles_elapsed());
 
 	for (i = 0; i < 8; i++) {
 	    printf("%02X ", cipher[i]);
@@ -79,28 +84,34 @@ int present80_wb()
 	int i;
 	clock_t program_start, program_end = 0;
 	program_start = clock();
+	set_cycles_start();
 
 	present_wb_init(key10, wb_ctx);
 
+	set_cycles_ends();
 	program_end = clock();
 	double ts = program_end - program_start;
 	ts = ts*1000/CLOCKS_PER_SEC;
 
 	printf("The generating of encryptions table spend %lf ms\n", ts);
+	printf("The generating of encryptions table spend %llu cycles\n", get_cycles_elapsed());
 	
 	program_start = clock();
+	set_cycles_start();
 
 	for(i = 0; i < EXPERIMENT_TIMES; i++)
 	{
 	    present_wb_enc(message, wb_ctx, cipher);
     }
 
+	set_cycles_ends();
 	program_end = clock();
 
 	ts = program_end - program_start;
 	ts = ts*1000/CLOCKS_PER_SEC;
 
 	printf("The %d times encryptions spend %lf ms\n", i, ts);
+	printf("The %d times encryptions spend %llu cycles\n", i, get_cycles_elapsed());
 
 	for (i = 0; i < 8; i++) {
 	    printf("%02X ", cipher[i]);
